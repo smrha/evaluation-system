530afb2ad640c098880b5214f3317e2d4d7f674a
smrha <lord.smrha@gmail.com>
2026-06-29 17:21:09 +0330

    [UPDATE] navbar and sidebar templates

--- account/views.py
+++ account/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 
-@login_required
+# @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
