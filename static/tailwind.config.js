0dd0d54d97efdff046f5cac6ab8d96c3b2829279
smrha <lord.smrha@gmail.com>
2026-06-30 11:12:12 +0330

    [ADD] logo favicon and fonts

---
+++ static/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        'vazir': ["Vazirmatn", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [],
+}
