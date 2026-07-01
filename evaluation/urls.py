f0c58e27a9b9232f5dbf17d59def77e5797fe9ac
smrha <lord.smrha@gmail.com>
2026-07-01 16:11:09 +0330

    [ADD] create and edit term view

--- evaluation/urls.py
+++ evaluation/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('terms/', views.terms_list, name='term_list'),
     path('terms/create/', views.create_term, name='create_term'),
+    path('terms/edit/<int:id>/', views.edit_term, name='edit_term'),
 ]
--- evaluation/views.py
+++ evaluation/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Terms
 from .forms import TermsForm
+
+def edit_term(request, id):
+    term = get_object_or_404(Terms, id=id)
+
+    if request.method == 'POST':
+        form = TermsForm(request.POST, instance=term)
+
+        if form.is_valid():
+            form.save()
+            return redirect("term_list")
+    else:
+        form = TermsForm(instance=term)
+    
+    context = {
+        'form': form,
+        'term': term,
+    }
+    return render(request, 'evaluation/term/edit.html', context)
 
 def create_term(request):
     if request.method == 'POST':
