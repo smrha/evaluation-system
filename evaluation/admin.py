a5d7cf856a6e7a7207675463bcd137d6d401d5f8
smrha <lord.smrha@gmail.com>
2026-07-01 10:57:03 +0330

    [ADD] answer and exam model

--- evaluation/admin.py
+++ evaluation/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question, Terms
+from .models import Question, Terms, Exam, Answer
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
@@ -9,3 +9,20 @@ class QuestionAdmin(admin.ModelAdmin):
 @admin.register(Terms)
 class TermsAdmin(admin.ModelAdmin):
     list_display = ['title', 'is_active']
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ['assessee', 'evaluator', 'term']
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['question_name', 'exam_name', 'value']
+
+    def question_name(self, obj):
+        return obj.question.title
+    
+    def exam_name(self, obj):
+        return f'فرم ارزیابی {obj.exam.assessee.first_name} {obj.exam.assessee.last_name}'
+    
+    question_name.short_description = "عنوان سوال"
+    exam_name.short_description = "دوره ارزیابی"
--- evaluation/models.py
+++ evaluation/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+
 class Terms(models.Model):
     title = models.CharField(max_length=128, verbose_name='دوره ارزیابی')
     is_active = models.BooleanField(default=0 ,verbose_name='وضعیت')
@@ -29,4 +31,22 @@ class Question(models.Model):
     def __str__(self):
         return self.title
 
+class Exam(models.Model):
+    assessee = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assessees', verbose_name='ارزیابی شونده')
+    evaluator = models.ForeignKey(User, models.CASCADE, related_name='evaloators',verbose_name='ارزیاب')
+    term = models.ForeignKey(Terms, on_delete=models.CASCADE, verbose_name='دوره ارزیابی')
     
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'فرم ارزیابی {self.assessee.first_name} {self.assessee.first_name}'
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='سوال')
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE, verbose_name='دوره ارزیابی')
+    value = models.IntegerField(default=0, verbose_name='امتیاز')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
