47ac63faca4ef13fd84c442d0a24983595758952
smrha <lord.smrha@gmail.com>
2026-07-01 13:32:15 +0330

    [ADD] term create and term list views

---
+++ evaluation/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('terms/', views.terms_list, name='term_list'),
+    path('terms/create/', views.create_term, name='create_term'),
+]
--- evaluation/views.py
+++ evaluation/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Terms
+from .forms import TermsForm
 
-# Create your views here.
+def create_term(request):
+    if request.method == 'POST':
+        form = TermsForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("term_list")
+    else:
+        form = TermsForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'evaluation/term/create.html', context)
+
+def terms_list(request):
+    terms = Terms.objects.all()
+    context = {
+        'terms': terms,
+    }
+    return render(request, 'evaluation/term/list.html', context)
